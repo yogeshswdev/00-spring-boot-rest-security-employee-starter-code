55ae943a341ab08cd362950824b621baa05473e6
Yogesh <yogeshkhot2007@gmail.com>
2023-05-28 18:57:28 +0530

    added custom username and authority table changes

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -42,7 +42,16 @@ public class DemoSecurityConfig {
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to get user name from custom table
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id = ?");
+
+        // define query to get authorities
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id = ?");
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
